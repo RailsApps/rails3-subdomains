17eb0cdc6afbfa3da4ab0cefcf08c0592a43587d
Daniel Kehoe <daniel@danielkehoe.com>
2011-11-12 12:18:57 -0800

    user name as a subdomain

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,6 +7,8 @@ class User
 
   field :name
   validates_presence_of :name
+  validates_format_of :name, with: /^[a-z0-9_]+$/, message: "must be lowercase alphanumerics only"
+  validates_length_of :name, maximum: 32, message: "exceeds maximum of 32 characters"
   validates_uniqueness_of :name, :email, :case_sensitive => false
   attr_accessible :name, :email, :password, :password_confirmation, :remember_me
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -8,5 +8,6 @@
 puts 'EMPTY THE MONGODB DATABASE'
 Mongoid.master.collections.reject { |c| c.name =~ /^system/}.each(&:drop)
 puts 'SETTING UP DEFAULT USER LOGIN'
-user = User.create! :name => 'First User', :email => 'user@example.com', :password => 'please', :password_confirmation => 'please'
+# user = User.create! :name => 'First User', :email => 'user@example.com', :password => 'please', :password_confirmation => 'please'
+user = User.create! :name => 'myname', :email => 'user@example.com', :password => 'please', :password_confirmation => 'please'
 puts 'New user created: ' << user.name
