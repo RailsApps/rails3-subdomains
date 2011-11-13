6fc7a4eebd632f15e359e2531a6b0d2070b5fb4d
Daniel Kehoe <daniel@danielkehoe.com>
2011-11-12 16:48:57 -0800

    implement routing for subdomains (with corrections)

--- app/controllers/profiles_controller.rb
+++ app/controllers/profiles_controller.rb
@@ -1,6 +1,10 @@
 class ProfilesController < ApplicationController
   def show
-    @user = User.first(conditions: { name: request.subdomain })
+    @user = User.first(conditions: { name: request.subdomain }) || not_found
+  end
+  
+  def not_found
+    raise ActionController::RoutingError.new('User Not Found')
   end
 
 end
