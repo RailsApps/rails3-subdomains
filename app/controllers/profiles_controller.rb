d4063ec36543386369f3c8cd481ee53b9a1ffcb2
Slava Markevich <slava.markevich@gmail.com>
2013-03-25 22:47:00 +0300

    use RecordNotFound Exception instead of RoutingError
    Model.first! raises RecordNotFound if no matching record is found.
    
    In production mode, Rails automatically rescues the ActiveRecord::RecordNotFound exception by rendering the 404 error page.

--- app/controllers/profiles_controller.rb
+++ app/controllers/profiles_controller.rb
@@ -1,9 +1,5 @@
 class ProfilesController < ApplicationController
   def show
-    @user = User.where(:name => request.subdomain).first || not_found
-  end
-  
-  def not_found
-    raise ActionController::RoutingError.new('User Not Found')
+    @user = User.where(:name => request.subdomain).first!
   end
 end
